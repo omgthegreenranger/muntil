235c3d8e12c6ae4196bc38a8a39bad5a60b76fad
omgthegreenranger <das.shaggy@gmail.com>
2023-05-12 13:32:26 -0400

    Sending up

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env: {
+        'MYSQL_HOST': '127.0.0.1',
+        'MYSQL_PORT': '3306',
+        'MYSQL_DATABASE': 'muntil_db',
+        'MYSQL_USER': 'muntil',
+        'MYSQL_PASSWORD': 'muntil',
+      }
+}
 
 module.exports = nextConfig
---
+++ src/app/display/layout.jsx
@@ -0,0 +1,3 @@
+export default function DisplayLayout({ children }) {
+    return <section>{children}</section>;
+  }
